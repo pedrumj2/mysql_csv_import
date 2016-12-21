7b50ba10f710a1368166f50c8e5c28c6f5e722bf
pedrum <pedrum.jalali3@gmail.com>
2016-12-21 17:43:59 +0000

    f

--- script.sh
+++ script.sh
@@ -5,16 +5,23 @@ if [ $1 == "-h" ]; then
   printf "note escape special characters in password\n"
 else
   NAME=$(basename $3);
- A=$(printf "mysql -u root -p'%s' %s -e \"CREATE TABLE IF NOT EXISTS %s(" $1 $2 $4)
-  B=$(head -1 $3 | sed -e 's/,/" "VARCHAR(20),/g'  | xargs printf "%s" $3)
+  echo $NAME;
+ A=$(printf "mysql -u root -p'%s' %s -e \"CREATE TABLE IF NOT EXISTS %s(" $1 $2 $NAME)
+  B=$(head -1 $3 | sed -e 's/,/ VARCHAR(20),/g')
   
   C=$(printf  " VARCHAR(20));\"\n")
-  eval $A $B $C
+ eval $A $B $C
+ #echo $C;
+# eval $B;
+  #$D=$(printf "%s" $B)
+ # echo $D;
+ # echo $3;
   
   
-  A=$(printf "mysqlimport --local --ignore-lines=1 --fields-terminated-by=, -u root -p'%s'\n\n %2 " $1 $3)
-  B=$(printf "%3\n" $1 $3)
-  eval $A $B
+  A=$(printf "mysqlimport --local --ignore-lines=1 --fields-terminated-by=, -u root -p'%s'\n\n %s " $1 $2)
+ B=$(printf "%s\n" $3)
+ 
+ eval $A $B
    echo $A
    echo $B
 fi
