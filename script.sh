1cf1261ce68e9e04a6f6d503ace135aa6711ac62
pedrumj2 <pedrum.jalali@gmail.com>
2017-06-21 20:53:06 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,24 +4,8 @@ if [ $1 == "-h" ]; then
   printf "Usage <Password> <database> <csv file>\n"
   printf "note escape special characters in password\n"
 else
-  NAME=$(basename $3);
-  echo $NAME;
- A=$(printf "mysql -u root -p'%s' %s -e \"CREATE TABLE IF NOT EXISTS %s(" $1 $2 $NAME)
-  B=$(head -1 $3 | sed -e 's/,/ VARCHAR(20),/g')
-  
-  C=$(printf  " VARCHAR(20));\"\n")
- eval $A $B $C
- #echo $C;
-# eval $B;
-  #$D=$(printf "%s" $B)
- # echo $D;
- # echo $3;
-  
-  
-  A=$(printf "mysqlimport --local --ignore-lines=1 --fields-terminated-by=, -u root -p'%s'\n\n %s " $1 $2)
- B=$(printf "%s\n" $3)
- 
- eval $A $B
-   echo $A
-   echo $B
+  NAME=$(echo $3 | cut -f 1 -d '.')
+  rest=$(head -1 $3 | sed -e 's/,/ VARCHAR(20),/g')
+  mysql -u root -p$1 $2 -e "CREATE TABLE IF NOT EXISTS ${NAME} ( ${rest}  VARCHAR(20))"  
+  mysqlimport --local --ignore-lines=1 --fields-terminated-by=, -u root -p$1 $2 $3
 fi
